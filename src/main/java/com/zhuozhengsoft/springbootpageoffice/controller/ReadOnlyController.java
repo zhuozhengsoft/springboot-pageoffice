e53073a7f367ecdb1f0c1572c5764eeedb9510da
董萍 <dp@zhuozhengsoft.com>
2022-08-31 16:08:06 +0800

    修改ReadOnly示例，去掉setAllowCopy，修改为禁止右键和禁止选中

--- src/main/java/com/zhuozhengsoft/springbootpageoffice/controller/ReadOnlyController.java
+++ src/main/java/com/zhuozhengsoft/springbootpageoffice/controller/ReadOnlyController.java
@@ -2,6 +2,7 @@ package com.zhuozhengsoft.springbootpageoffice.controller;
 
 import com.zhuozhengsoft.pageoffice.OpenModeType;
 import com.zhuozhengsoft.pageoffice.PageOfficeCtrl;
+import com.zhuozhengsoft.pageoffice.wordwriter.WordDocument;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,15 +18,18 @@ public class ReadOnlyController {
 
     @RequestMapping(value = "Word", method = RequestMethod.GET)
     public ModelAndView showWord(HttpServletRequest request, Map<String, Object> map) {
+		WordDocument wordDoc=new WordDocument();
+        doc.setDisableWindowSelection(true);//禁止选中
+        doc.setDisableWindowRightClick(true);//禁止右键
         PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
         poCtrl.setServerPage(request.getContextPath() + "/poserver.zz");//设置服务页面
-        poCtrl.setAllowCopy(false);//禁止拷贝
         poCtrl.setMenubar(false);//隐藏菜单栏
         poCtrl.setOfficeToolbars(false);//隐藏Office工具条
         poCtrl.setCustomToolbar(false);//隐藏自定义工具栏
         poCtrl.setJsFunction_AfterDocumentOpened("AfterDocumentOpened");
         //设置页面的显示标题
         poCtrl.setCaption("演示：文件在线安全浏览");
+		poCtrl1.setWriter(doc);//注意：使用WordDocument类后，此句必须
         //打开Word文档
         poCtrl.webOpen("/doc/ReadOnly/test.doc", OpenModeType.docNormalEdit, "张三");
         map.put("pageoffice", poCtrl.getHtmlCode("PageOfficeCtrl1"));
